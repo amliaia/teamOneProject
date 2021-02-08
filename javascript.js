6cfede046ef2aff3c88a92c6553e0c9f6e1eb05b
Amelia Sanchez <ameliaivilo@gmail.com>
2021-02-08 17:45:09 -0600

    matched proper id's for the GetElementbyID

---
+++ javascript.js
@@ -0,0 +1,28 @@
+var fetchButton = document.getElementById("#button1");
+var termEl = document.getElementById("#input");
+
+// var fetchButton = document.getElementById("submit");
+// var termEl = document.getElementById("user-search")
+
+function searchUp(e) {
+  var query = termEl.value;
+  console.log(query);
+
+  fetch("https://api.giphy.com/v1/gifs/search?" + query + "&api_key=uwfGVryAAQRWE03fz66tfFjbFGjQdJ4f&limit=10")
+
+  .then(response => {
+      console.log(response);
+      return response.json();
+      
+  })
+  .then(data)
+}
+
+var fetchButton = document.getElementById('#button1');
+if(fetchButton){
+  fetchButton.addEventListener('click', searchUp);
+}
+
+
+
+
